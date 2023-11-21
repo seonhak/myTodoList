4656f0e8930f1fb967bb1d18d8e99ae1462150a7
seonhak1~ <tjsgkr134679@gmail.com>
2023-11-21 09:56:54 +0900

    jwt 토큰인증, 쿠키추가 구현 중

--- src/main/java/com/seonhak/mytodolist/controller/AuthController.java
+++ src/main/java/com/seonhak/mytodolist/controller/AuthController.java
@@ -1,2 +1,86 @@
-package com.seonhak.mytodolist.controller;public class AuthController {
+package com.seonhak.mytodolist.controller;
+
+import com.seonhak.mytodolist.entity.User;
+import com.seonhak.mytodolist.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class AuthController {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    private final JwtUtil jwtUtil;
+
+    public AuthController(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @GetMapping("/create-jwt")
+    public String createJwt(HttpServletResponse res) {
+        // Jwt 생성
+        String token = jwtUtil.createToken("Robbie", User.USER);
+
+        // Jwt 쿠키 저장
+        jwtUtil.addJwtToCookie(token, res);
+
+        return "createJwt : " + token;
+    }
+
+    @GetMapping("/get-jwt")
+    public String getJwt(@CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue) {
+        // JWT 토큰 substring
+        String token = jwtUtil.substringToken(tokenValue);
+
+        // 토큰 검증
+        if (!jwtUtil.validateToken(token)) {
+            throw new IllegalArgumentException("Token Error");
+        }
+
+        // 토큰에서 사용자 정보 가져오기
+        Claims info = jwtUtil.getUserInfoFromToken(token);
+        // 사용자 username
+        String username = info.getSubject();
+        System.out.println("username = " + username);
+        // 사용자 권한
+        String authority = (String) info.get(JwtUtil.AUTHORIZATION_KEY);
+        System.out.println("authority = " + authority);
+
+        return "getJwt : " + username + ", " + authority;
+    }
+
+    @GetMapping("/create-cookie")
+    public String createCookie(HttpServletResponse res) {
+        addCookie("Robbie Auth", res);
+
+        return "createCookie";
+    }
+
+    @GetMapping("/get-cookie")
+    public String getCookie(@CookieValue(AUTHORIZATION_HEADER) String value) {
+        System.out.println("value = " + value);
+
+        return "getCookie : " + value;
+    }
+
+    public static void addCookie(String cookieValue, HttpServletResponse res) {
+        try {
+            cookieValue = URLEncoder.encode(cookieValue, "utf-8").replaceAll("\\+", "%20"); // Cookie Value 에는 공백이 불가능해서 encoding 진행
+
+            Cookie cookie = new Cookie(AUTHORIZATION_HEADER, cookieValue); // Name-Value
+            cookie.setPath("/");
+            cookie.setMaxAge(30 * 60);
+
+            // Response 객체에 Cookie 추가
+            res.addCookie(cookie);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
+
--- src/main/java/com/seonhak/mytodolist/entity/User.java
+++ src/main/java/com/seonhak/mytodolist/entity/User.java
@@ -1,2 +1,25 @@
-package com.seonhak.mytodolist.entity;public class User {
+package com.seonhak.mytodolist.entity;
+
+import lombok.Getter;
+
+public enum User {
+    USER(Authority.USER),  // 사용자 권한
+    ADMIN(Authority.ADMIN);  // 관리자 권한
+
+    @Getter
+    private final String authority;
+    private String password;
+    User(String authority) {
+        this.authority = authority;
+    }
+
+    User(String authority, String password) {
+        this.authority = authority;
+        this.password = password;
+    }
+
+    public static class Authority {
+        public static final String USER = "ROLE_USER";
+        public static final String ADMIN = "ROLE_ADMIN";
+    }
 }
